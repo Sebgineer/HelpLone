7b68245b44c94c2aa1135b4181c59a5d916e147c
seba6474 <seba6474@ZBCAA-SEBA6474>
2021-12-09 09:31:26 +0100

    Add project files.

---
+++ HelpLone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelpLone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
